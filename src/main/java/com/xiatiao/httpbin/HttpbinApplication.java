d9a4cd1dbdb3888c5907740311882bfd639e5098
wych42 <wych101010@gmail.com>
2020-10-17 00:14:30 +0800

    add swagger-ui

--- src/main/java/com/xiatiao/httpbin/HttpbinApplication.java
+++ src/main/java/com/xiatiao/httpbin/HttpbinApplication.java
@@ -2,12 +2,45 @@ package com.xiatiao.httpbin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class HttpbinApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HttpbinApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HttpbinApplication.class, args);
+    }
 
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .tags(
+                        new Tag("Request Inspection", "Inspect the request data")
+                )
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.xiatiao.httpbin"))
+                .paths(PathSelectors.regex("/.*"))
+                .build()
+                .pathMapping("/");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Httpbin")
+                .description("A simple HTTP Request & Response Service.")
+                .version("2.0")
+                .build();
+    }
 }
